99b03668eb1a227870f3b7199494df89dfc5cb7f
Douglas Alves <douglas.expalves@gmail.com>
2017-11-16 11:20:02 -0200

    chat feature working

--- src/components/Chat.js
+++ src/components/Chat.js
@@ -1,4 +1,9 @@
 import React, { Component } from 'react'
+import io from 'socket.io-client'
+import { config } from 'dotenv'
+
+config()
+console.log(process.env)
 
 class Chat extends Component {
   constructor (props) {
@@ -8,6 +13,31 @@ class Chat extends Component {
       message: '',
       messages: []
     }
+    this.socket = io(`192.168.15.119:3080`)
+    this.socket.on('RECEIVE_MESSAGE', data => {
+      this.addMessage(data)
+    })
+    this.sendMessage = this.sendMessage.bind(this)
+  }
+
+  sendMessage (e) {
+    e.preventDefault()
+    this.socket.emit('SEND_MESSAGE', {
+      author: this.state.username,
+      message: this.state.message
+    })
+
+    this.setState({
+      message: ''
+    })
+  }
+
+  addMessage (data) {
+    console.log(data)
+    this.setState({
+      messages: [...this.state.messages, data]
+    })
+    console.log(this.state.messages)
   }
 
   render () {
@@ -20,17 +50,36 @@ class Chat extends Component {
                 <div className='card-title'>Chat App</div>
                 <hr />
                 <div className='messages'>
-                  {this.state.messages.map(message =>
-                    <div>{message.author}: {message.message}</div>
-                    )}
+                  {this.state.messages.map(message => (
+                    <div>
+                      {message.author}: {message.message}
+                    </div>
+                  ))}
                 </div>
               </div>
               <div className='card-footer'>
-                <input type='text' placeholder='Username' className='form-control' value={this.state.username} onChange={ev => this.setState({username: ev.target.value})} />
+                <input
+                  type='text'
+                  placeholder='Username'
+                  className='form-control'
+                  value={this.state.username}
+                  onChange={ev => this.setState({ username: ev.target.value })}
+                />
                 <br />
-                <input type='text' placeholder='Message' className='form-control' value={this.state.message} onChange={ev => this.setState({message: ev.target.value})} />
+                <input
+                  type='text'
+                  placeholder='Message'
+                  className='form-control'
+                  value={this.state.message}
+                  onChange={ev => this.setState({ message: ev.target.value })}
+                />
                 <br />
-                <button className='btn btn-primary form-control'>Send</button>
+                <button
+                  className='btn btn-primary form-control'
+                  onClick={this.sendMessage}
+                >
+                  Enviar
+                </button>
               </div>
             </div>
           </div>
