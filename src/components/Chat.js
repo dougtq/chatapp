251a00d966c6c125eb4b0e5c0e58d647f9318d83
Douglas Alves <douglas.expalves@gmail.com>
2017-11-16 08:41:54 -0200

    initial chat structure

---
+++ src/components/Chat.js
@@ -0,0 +1,43 @@
+import React, { Component } from 'react'
+
+class Chat extends Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      username: '',
+      message: '',
+      messages: []
+    }
+  }
+
+  render () {
+    return (
+      <div className='container'>
+        <div className='row'>
+          <div className='col-4'>
+            <div className='card'>
+              <div className='card-body'>
+                <div className='card-title'>Global Chat App</div>
+                <hr />
+                <div className='messages'>
+                  {this.state.messages.map(message =>
+                    <div>{message.author}: {message.message}</div>
+                    )}
+                </div>
+              </div>
+              <div className='card-footer'>
+                <input type='text' placeholder='Username' className='form-control' value={this.state.username} onChange={ev => this.setState({username: ev.target.value})} />
+                <br />
+                <input type='text' placeholder='Message' className='form-control' value={this.state.message} onChange={ev => this.setState({message: ev.target.value})} />
+                <br />
+                <button className='btn btn-primary form-control'>Send</button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default Chat
